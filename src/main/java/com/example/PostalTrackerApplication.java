9a5b0d016c5416dcd8412e48e94cf822f138e3c4
Nick Kisel <insherlolqq@gmail.com>
2022-05-21 19:04:18 +0300

    prepare project

---
+++ src/main/java/com/example/PostalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostalTrackerApplication.class, args);
+    }
+
+}
